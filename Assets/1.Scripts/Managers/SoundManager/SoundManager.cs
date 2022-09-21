7ad028bbded83bdd91fe9b50a19b69e6257730f8
Lee_Jae_Hwan <ok17jh@gmail.com>
2022-09-21 09:42:04 +0900

    Debug  Test

--- Assets/1.Scripts/Managers/SoundManager/SoundManager.cs
+++ Assets/1.Scripts/Managers/SoundManager/SoundManager.cs
@@ -7,7 +7,7 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // debug
     }
 
     // Update is called once per frame
